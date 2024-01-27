112c4448cc64d965377c8c35fdbe04e8fff14154
YACINE <yacine@MacBook-Pro-de-YACINE.local>
2024-01-27 04:17:55 +0000

    c'est le disign du prestataire

--- resources/views/layouts/gmao/prestataire/sidebar.blade.php
+++ resources/views/layouts/gmao/prestataire/sidebar.blade.php
@@ -1,25 +1,25 @@
 <ul class="py-1 menu-inner">
     <!-- Dashboards -->
-    <x-gmao-nav-link :active="request()->routeIs('demandeur.dashboard')">
-        <a href="{{ route('demandeur.dashboard') }}" class="menu-link">
+    <x-gmao-nav-link :active="request()->routeIs('prestataires.dashboard')">
+        <a href="{{ route('prestataires.dashboard') }}" class="menu-link">
             <i class="menu-icon tf-icons ti ti-home"></i>
             <div data-i18n="Tableau de Bord">Tableau de Bord</div>
         </a>
     </x-gmao-nav-link>
 
     <!-- Demandes -->
-    <x-gmao-nav-link :active="request()->routeIs('demandeur.demandes.*')">
-        <a href="{{ route('demandeur.demandes.index') }}" class="menu-link">
+    <x-gmao-nav-link :active="request()->routeIs('prestataires.demandes.*')">
+        <a href="{{ route('prestataires.demandes.index') }}" class="menu-link">
             <i class="menu-icon fa-solid fa-hand-holding-medical"></i>
             <div data-i18n="Demandes">Demandes</div>
         </a>
     </x-gmao-nav-link>
 
     <!-- Demandes -->
-    <x-gmao-nav-link :active="request()->routeIs('demandeur.sites.*')">
-        <a href="{{ route('demandeur.sites.index') }}" class="menu-link">
-            <i class="menu-icon fa-solid fa-screwdriver-wrench"></i>
-            <div data-i18n="Sites">Sites</div>
+    <x-gmao-nav-link :active="request()->routeIs('prestataires.utilisateurs.*')">
+        <a href="{{ route('prestataires.utilisateurs.index') }}" class="menu-link">
+            <i class="menu-icon fa-solid fa-users"></i>
+            <div data-i18n="Utilisateurs">Utilisateurs</div>
         </a>
     </x-gmao-nav-link>
 </ul>
