1d083295715b7c4e8826233692443c20b98b4a92
yelemaTech <dev@yelema.tech>
2024-02-14 19:46:32 +0000

    Equipement -mfc -r

---
+++ database/migrations/2024_02_14_084848_create_equipements_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('equipements', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('categorie');
+            $table->string('numero_serie');
+            $table->double('forfait_contrat');
+            $table->index('site_id');
+            $table->foreign('site_id')->references('id')->on('sites')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('equipements');
+    }
+};
