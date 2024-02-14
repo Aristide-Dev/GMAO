87a69b132536173548c17f174ab6433dee1967bd
yelemaTech <dev@yelema.tech>
2024-02-14 19:52:46 +0000

    Demande Intervention -mfc -r

---
+++ app/Http/Controllers/DemandeInterventionController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DemandeIntervention;
+use Illuminate\Http\Request;
+
+class DemandeInterventionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(DemandeIntervention $demandeIntervention)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(DemandeIntervention $demandeIntervention)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, DemandeIntervention $demandeIntervention)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(DemandeIntervention $demandeIntervention)
+    {
+        //
+    }
+}
