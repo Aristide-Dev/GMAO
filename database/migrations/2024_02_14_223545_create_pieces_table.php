5983bfed2b681246afe5f97233a6f8df4dfe9038
yelemaTech <dev@yelema.tech>
2024-02-14 22:39:08 +0000

    Piece -mfc -r

---
+++ database/migrations/2024_02_14_223545_create_pieces_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pieces', function (Blueprint $table) {
+            $table->id();
+            $table->string("piece");
+            $table->integer("price")->default(0);
+            $table->integer("quantite")->default(0);
+            $table->integer("stock_min")->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pieces');
+    }
+};
