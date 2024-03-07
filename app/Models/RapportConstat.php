be60812570c5644f98a6f9a9048123da6d5a62de
yelemaTech <dev@yelema.tech>
2024-03-07 18:06:11 +0000

    complete db info  for rapport constat

--- app/Models/RapportConstat.php
+++ app/Models/RapportConstat.php
@@ -3,9 +3,43 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Database\Eloquent\Model;
 
 class RapportConstat extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'bt_reference',
+        'rapport_constat_file',
+        'status',
+    ];
+
+
+
+    public function bon_travail()
+    {
+        return $this->belongsTo(BonTravail::class, 'bt_reference', 'bt_reference');
+    }
+
+    public function document()
+    {
+        // Vérifiez d'abord si la demande a un fichier associé
+        if ($this->rapport_constat_file) {
+            // Générez l'URL complète du fichier en utilisant Storage::url()
+            try {
+                return Storage::url($this->rapport_constat_file);
+            } catch (\Throwable $th) {
+                return null;
+            }
+        }
+        // Retournez null si aucun fichier n'est associé à la demande
+        return null;
+    }
 }
--- database/migrations/2024_02_14_230702_create_rapport_constats_table.php
+++ database/migrations/2024_02_14_230702_create_rapport_constats_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('rapport_constats', function (Blueprint $table) {
             $table->id();
+            $table->string("bt_reference");
+            $table->string('rapport_constat_file')->nullable();
+            $table->string('status');
             $table->timestamps();
+
+            $table->foreign('bt_reference')->references('bt_reference')->on('bon_travails')->onDelete('cascade');
         });
     }
 
