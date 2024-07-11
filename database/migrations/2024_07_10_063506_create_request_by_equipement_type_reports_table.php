79c74621be6f46bc3314a7ed2e3195ace86b12fc
yelemaTech <dev@yelema.tech>
2024-07-11 01:54:22 +0000

    fixe forgn key bug to request_by_equipement_type_reports_table

--- database/migrations/2024_07_10_063506_create_request_by_equipement_type_reports_table.php
+++ database/migrations/2024_07_10_063506_create_request_by_equipement_type_reports_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('request_by_equipement_type_reports', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('monthly_report_id')->constrained()->onDelete('cascade');
+            $table->foreignId('monthly_report_id')
+                ->constrained('monthly_reports')
+                ->onDelete('cascade')
+                ->name('req_equip_type_reports_monthly_report_id_fk'); // Nom de la contrainte de clé étrangère spécifié
             $table->string('etiquette');
             $table->integer('nb_requete');
             $table->timestamps();
@@ -25,6 +28,9 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::table('request_by_equipement_type_reports', function (Blueprint $table) {
+            $table->dropForeign('req_equip_type_reports_monthly_report_id_fk'); // Nom de la contrainte de clé étrangère spécifié
+        });
         Schema::dropIfExists('request_by_equipement_type_reports');
     }
 };
