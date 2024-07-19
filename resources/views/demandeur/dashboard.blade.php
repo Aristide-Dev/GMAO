54c9152a0a66c25996f5a4e4a72e72b652582901
Aristide-Dev <aristidegnimassouu@gmail.com>
2024-07-19 21:03:32 +0000

    correct dashbordsss

--- resources/views/demandeur/dashboard.blade.php
+++ resources/views/demandeur/dashboard.blade.php
@@ -8,63 +8,43 @@
         </h2>
     </x-slot>
 
+
+    @php
+    $userImageIllustration = ["woman.jpg", "boy.jpg"];
+    @endphp
+
+    <div class="row">
+        <div class="mb-4 col-md-12 h-72">
+            <div class="bg-center bg-no-repeat bg-cover border shadow shadow-red-500 size-full" style="background-image: url('{{ Storage::url("assets/img/sog/".$userImageIllustration[rand(0,1)]) }}');">
+                <div class="h-full mx-auto rounded-lg row">
+                    <div class="mb-3 rounded-lg col-12 h-72">
+                        <div class="pb-3 mb-3 card-body text-start ps-sm-0">
+                            <!-- Contenu de la carte ici -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div class="row">
         <!-- View sales -->
         <div class="mb-4 col-xl-4 col-lg-5 col-12">
-          <div class="card">
-            <div class="d-flex align-items-end row">
-              <div class="col-7">
-                <div class="card-body text-nowrap">
-                  <h5 class="mb-0 card-title">Content de vous revoir! 🎉</h5>
-                  <p class="mb-2">{{ fake()->name }}</p>
-                  <h4 class="mb-1 text-primary">$48.9k</h4>
-                  <a href="javascript:;" class="btn btn-primary">View Sales</a>
-                </div>
+          <div class="card bg-gradient-to-r from-blue-300 to-black">
+              <div class="d-flex align-items-end row">
+                  <div class="col-12">
+                      <div class="card-body text-nowrap">
+                          <h5 class="py-2 mb-4 font-bold text-white shadow-lg ps-2 card-title animate-pulse">Content de vous revoir! 🎉</h5>
+                          {{-- <p class="mb-2">{{ Auth::user()?->first_name }} {{ Auth::user()?->last_name }}</p> --}}
+                          {{-- <h4 class="mb-1 text-primary">$48.9k</h4> --}}
+                          <div class="mt-4 font-bold text-white bg-yellow-400 shadow hover:text-white hover:bg-yellow-500 btn">
+                              {{ Auth::user()?->first_name }} {{ Auth::user()?->last_name }}
+                          </div>
+                      </div>
+                  </div>
               </div>
-              <div class="text-center col-5 text-sm-left">
-                <div class="px-0 pb-0 card-body px-md-4">
-                    @php
-                        $userImageIllustration = ["card-advance-sale.png", "add-new-roles.png"];
-                    @endphp
-                  <img src="/storage/assets/img/illustrations/{{ $userImageIllustration[rand(0,1)] }}" height="140" alt="view sales">
-                </div>
-              </div>
-            </div>
           </div>
-        </div>
-        <!-- View sales -->
-
-        <!-- Statistics -->
-        <livewire:stats-card />
-        <!--/ Statistics -->
       </div>
-      <div class="row">
-        <div class="p-3 my-3 col-12">
-            <livewire:evolution-requetes />
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="p-3 my-3 shadow col-12">
-            <livewire:top-10-pannes />
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="p-3 my-3 shadow col-12">
-            <livewire:requete-by-equipement-type />
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="p-3 my-3 shadow col-12">
-            <livewire:requete-by-zone />
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="p-3 my-3 shadow col-12">
-            {{-- <livewire:cout-total-maintenance-by-site /> --}}
-        </div>
+        <!-- View sales -->
     </div>
 </x-gmao-layout>
