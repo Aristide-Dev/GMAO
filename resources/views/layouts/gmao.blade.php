017cf7045b6d4bdbd97f35b134e99ccbaed32aa1
Aristide-Dev <aristidegnimassouu@gmail.com>
2024-07-21 05:07:24 +0000

    icons

--- resources/views/layouts/gmao.blade.php
+++ resources/views/layouts/gmao.blade.php
@@ -8,13 +8,9 @@
     <title> {!! $title ? $title." | " : '' !!} {{ config('app.name', 'G-MAINTENANCE') }}</title>
     <link rel="icon" href="{{ asset('favicon.ico') }}">
     <!-- Fonts -->
-    {{-- <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" /> --}}
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com/">
-    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&amp;ampdisplay=swap" rel="stylesheet">
 
     <!-- Icons -->
     {{-- <link rel="stylesheet" href="/storage/assets/vendor/fonts/fontawesome.css" />
