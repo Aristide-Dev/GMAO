bbf579da317cf2ae3c123ff8c05db60360bbfd59
Aristide-Dev <aristidegnimassouu@gmail.com>
2024-08-08 12:36:44 +0000

    suppression des bt

--- resources/views/livewire/demandes-table.blade.php
+++ resources/views/livewire/demandes-table.blade.php
@@ -64,10 +64,7 @@
                         <div class="flex gap-1 flex-rows">
                             <a href="{{ route($url.'.demandes.show', $demande) }}" class="py-2 btn btn-primary">Suivis</a>
                             @if ($canDelete)
-                            <form method="POST" wire:submit.prevent='deleteDemande({{ $demande->id }})'>
-                                @csrf
-                                <button type="submit" class="text-white bg-red-500 btn hover:bg-red-600">Supprimer</button>
-                            </form>
+                                <button type="submit" wire:click.prevent='deleteDemande({{ $demande->id }})' class="text-white bg-red-500 btn hover:bg-red-600">Supprimer</button>
                             @endif
                         </div>
                     </td>
