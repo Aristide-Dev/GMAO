3de631c4bbddab11cd742f2d493c6fd37bdda2c9
Aristide-Dev <aristidegnimassouu@gmail.com>
2024-08-08 11:24:14 +0000

    add pays name to config

--- config/app.php
+++ config/app.php
@@ -18,6 +18,8 @@ return [
 
     'name' => env('APP_NAME', 'G-MAINTENANCE'),
     
+    'pays_name' => env('PAYS_NAME', 'STAR OIL'),
+    
     /*
     |--------------------------------------------------------------------------
     | Application Version
