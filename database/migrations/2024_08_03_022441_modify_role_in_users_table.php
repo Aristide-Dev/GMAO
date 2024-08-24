27886634d4bd90b557769f1d74f4e02579dd9380
Aristide-Dev <aristidegnimassouu@gmail.com>
2024-08-24 14:50:09 +0000

    Fix migration error by adding missing Fortify class

--- database/migrations/2024_08_03_022441_modify_role_in_users_table.php
+++ database/migrations/2024_08_03_022441_modify_role_in_users_table.php
@@ -1,9 +1,10 @@
 <?php
+use Laravel\Fortify\Fortify;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
